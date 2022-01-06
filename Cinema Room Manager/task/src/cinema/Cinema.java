dbe47d909bef08f7b7fa82ca63905723cbbeef8c
Maksim Korelov <korelov@gmail.com>
2022-01-06 15:53:06 +0300

    Stage 1/5: Arrangement  - Done

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,8 +1,23 @@
 package cinema;
 
+
 public class Cinema {
 
+    public static void room() {
+        String[][] seat = new String[7][8];
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        int row = 1;
+        for (int i = 0; i < seat.length; i++) {
+            System.out.print(row++ + " ");
+            for (int j = 0; j < seat[i].length; j++) {
+                System.out.print(seat[i][j] = "S" + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        // Write your code here
+        room();
     }
 }
